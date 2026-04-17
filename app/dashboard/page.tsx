c64837d2280796dfefeb4f9b8bfd7cf61640e25d
Ashif Sherin <ashifpkali@gmail.com>
2026-04-17 20:03:42 +0300

    save user to supabase

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,12 @@
 "use client";
 
 import { useEffect, useState } from "react";
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient(
+  "https://ssctpvqpszglytteytjp.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNzY3RwdnFwc3pnbHl0dGV5dGpwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYyMzc0NTIsImV4cCI6MjA5MTgxMzQ1Mn0.2a_QpNDw7vPrYe1OkFje3SdwF3gaEWCEn7iqca54sZQ"
+);
 
 export default function Dashboard() {
   const [user, setUser] = useState<string | null>(null);
@@ -12,6 +18,11 @@ export default function Dashboard() {
       window.location.href = "/login";
     } else {
       setUser(savedUser);
+
+      // 🔥 SAVE TO DATABASE
+      supabase.from("waitlist").insert([
+        { email: savedUser }
+      ]);
     }
   }, []);
 
@@ -20,6 +31,8 @@ export default function Dashboard() {
   return (
     <div style={{ padding: "50px" }}>
       <h1>Welcome {user}</h1>
+
+      <p>User saved to database 🚀</p>
 
       <button
         onClick={() => {
