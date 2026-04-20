a6aca7cd053095614bdf0c775069f262a2404009
Ashif Sherin <ashifpkali@gmail.com>
2026-04-20 11:33:54 +0300

    fix api key

--- app/invoice/page.tsx
+++ app/invoice/page.tsx
@@ -7,7 +7,7 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(
   "https://ssctpvqpszglytteytjp.supabase.co",
-  "sb_publishable_jHLki07GcaZDH3fQz7cFlg_9s57meZS
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNzY3RwdnFwc3pnbHl0dGV5dGpwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYyMzc0NTIsImV4cCI6MjA5MTgxMzQ1Mn0.2a_QpNDw7vPrYe1OkFje3SdwF3gaEWCEn7iqca54sZQ"
 "
 );
 
