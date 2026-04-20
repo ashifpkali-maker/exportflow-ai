0b2e22991bf4a52b597b73536c7dfc3cd69c8a3e
Ashif Sherin <ashifpkali@gmail.com>
2026-04-20 11:31:22 +0300

    fix supabase key

--- app/invoice/page.tsx
+++ app/invoice/page.tsx
@@ -7,7 +7,8 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(
   "https://ssctpvqpszglytteytjp.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNzY3RwdnFwc3pnbHl0dGV5dGpwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYyMzc0NTIsImV4cCI6MjA5MTgxMzQ1Mn0.2a_QpNDw7vPrYe1OkFje3SdwF3gaEWCEn7iqca54sZQYOUR_PUBLISHABLE_KEY_HERE"
+  "sb_publishable_jHLki07GcaZDH3fQz7cFlg_9s57meZS
+"
 );
 
 type Invoice = {
